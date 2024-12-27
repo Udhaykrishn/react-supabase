e1c7c6ef75cd6669a8470be12dd0f3b9d4471998
arjuxlal <arjunlal543k210@gmail.com>
2024-12-27 18:33:57 +0530

    protected the urls

--- src/supabase/supabase.jsx
+++ src/supabase/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tcduevvsrcmipwbvjjej.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZHVldnZzcmNtaXB3YnZqamVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyMTU2MTksImV4cCI6MjA1MDc5MTYxOX0.CQUMieVVRg715y3altSOn4AIJwcEU-LcSNIoxjaSmrY';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
